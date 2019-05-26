ab6b1894b4ee840c53de535f3d32773fc42ce1e1
anders-i <anders_i@hotmail.com>
2019-05-26 13:23:43 +0200

    logger + token check added to all methods

--- src/main/java/io/swagger/api/impl/LocationApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/LocationApiServiceImpl.java
@@ -3,6 +3,7 @@ package io.swagger.api.impl;
 import dk.aiae.client.AuthManagementClient;
 import implementation.DataSource;
 import implementation.DatabaseController;
+import implementation.TokenConverter;
 import io.swagger.api.*;
 import io.swagger.model.*;
 
@@ -16,45 +17,73 @@ import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
 import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.validation.constraints.*;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2019-05-26T10:45:56.712Z")
 public class LocationApiServiceImpl extends LocationApiService {
+
     public LocationApiServiceImpl() {
         AuthManagementClient.setBasePath("http://127.0.0.1:30000/authenticationManagement");
     }
-    
+
     @Override
     public Response addLocation(LocationRequest body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            new DatabaseController().addLocation(body.getLocation().getName().trim(), con);
-            return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-        } catch (Exception e) {
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body.getToken()));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                new DatabaseController().addLocation(body.getLocation().getName().trim(), con);
+                return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+            } catch (Exception e) {
+                Logger.getLogger(LocationApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(LocationApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
             return Response.status(400).entity(e.toString()).build();
         }
     }
+
     @Override
     public Response deleteLocation(LocationRequest body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            new DatabaseController().deleteLocation(body.getLocation().getName().trim(), con);
-            return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-        } catch (Exception e) {
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body.getToken()));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                new DatabaseController().deleteLocation(body.getLocation().getName().trim(), con);
+                return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+            } catch (Exception e) {
+                Logger.getLogger(LocationApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(LocationApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
             return Response.status(400).entity(e.toString()).build();
         }
     }
+
     @Override
     public Response getAllLocations(Token body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            AllLocations response = new DatabaseController().getAllLocations(con);
-            return Response.ok().entity(response).build();
-        } catch (Exception e) {
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                AllLocations response = new DatabaseController().getAllLocations(con);
+                return Response.ok().entity(response).build();
+            } catch (Exception e) {
+                Logger.getLogger(LocationApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(LocationApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
             return Response.status(400).entity(e.toString()).build();
         }
     }
+
     @Override
     public Response searchLocation(LocationSearchRequest body, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
--- src/main/java/io/swagger/api/impl/ProductsApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/ProductsApiServiceImpl.java
@@ -33,28 +33,44 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.validation.constraints.*;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2019-05-26T10:45:56.712Z")
 public class ProductsApiServiceImpl extends ProductsApiService {
+
     public ProductsApiServiceImpl() {
         AuthManagementClient.setBasePath("http://127.0.0.1:30000/authenticationManagement");
     }
 
     @Override
     public Response addCategory(CategoryRequest body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            new DatabaseController().addNewCategory(body.getCategory(), con);
-            return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-        } catch (Exception e) {
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body.getToken()));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                new DatabaseController().addNewCategory(body.getCategory(), con);
+                return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+            } catch (Exception e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
             return Response.status(400).entity(e.toString()).build();
         }
     }
 
     @Override
     public Response addProduct(ProductRequest body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            new DatabaseController().createProduct(body.getProduct(), con);
-            return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-        } catch (Exception e) {
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body.getToken()));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                new DatabaseController().createProduct(body.getProduct(), con);
+                return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+            } catch (Exception e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
             return Response.status(400).entity(e.toString()).build();
         }
     }
@@ -67,6 +83,7 @@ public class ProductsApiServiceImpl extends ProductsApiService {
                 new DatabaseController().deleteProduct(body.getProduct(), con);
                 return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
             } catch (Exception e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
                 return Response.status(400).entity(e.toString()).build();
             }
         } catch (io.swagger.client.ApiException e) {
@@ -77,10 +94,17 @@ public class ProductsApiServiceImpl extends ProductsApiService {
 
     @Override
     public Response editLocationOnMultipleProducts(EditListOfProductsRequest body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            new DatabaseController().editLocationOnMultipleProducts(body, con);
-            return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-        } catch (Exception e) {
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body.getToken()));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                new DatabaseController().editLocationOnMultipleProducts(body, con);
+                return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+            } catch (Exception e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
             return Response.status(400).entity(e.toString()).build();
         }
     }
@@ -104,51 +128,86 @@ public class ProductsApiServiceImpl extends ProductsApiService {
 
     @Override
     public Response getAllCategories(Token body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            AllCategories response = new DatabaseController().getAllCategories(con);
-            return Response.ok().entity(response).build();
-        } catch (SQLException ex) {
-            return Response.status(400).entity(ex.toString()).build();
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                AllCategories response = new DatabaseController().getAllCategories(con);
+                return Response.ok().entity(response).build();
+            } catch (SQLException e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+            return Response.status(400).entity(e.toString()).build();
         }
     }
 
     @Override
     public Response getAllProducts(Token body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            ProductArray response = new DatabaseController().getAllProducts(con);
-            return Response.ok().entity(response).build();
-        } catch (SQLException ex) {
-            return Response.status(400).entity(ex.toString()).build();
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                ProductArray response = new DatabaseController().getAllProducts(con);
+                return Response.ok().entity(response).build();
+            } catch (SQLException e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+            return Response.status(400).entity(e.toString()).build();
         }
     }
 
     @Override
     public Response getAllProductsOnLocation(AllProductsOnLocationRequest body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            ProductArray response = new DatabaseController().getAllProductsOnLocation(body.getLocation().getBarcodeID(), con);
-            return Response.ok().entity(response).build();
-        } catch (SQLException ex) {
-            return Response.status(400).entity(ex.toString()).build();
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body.getToken()));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                ProductArray response = new DatabaseController().getAllProductsOnLocation(body.getLocation().getBarcodeID(), con);
+                return Response.ok().entity(response).build();
+            } catch (SQLException e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+            return Response.status(400).entity(e.toString()).build();
         }
     }
 
     @Override
     public Response getProduct(GetProductRequest body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            Product response = new DatabaseController().getProduct(body.getProduct().getBarcodeID(), con);
-            return Response.ok().entity(response).build();
-        } catch (SQLException ex) {
-            return Response.status(400).entity(ex.toString()).build();
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body.getToken()));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                Product response = new DatabaseController().getProduct(body.getProduct().getBarcodeID(), con);
+                return Response.ok().entity(response).build();
+            } catch (SQLException e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+            return Response.status(400).entity(e.toString()).build();
         }
     }
 
     @Override
     public Response searchProducts(SearchRequest body, SecurityContext securityContext) throws NotFoundException {
-        try (Connection con = DataSource.getInstance().getConnection()) {
-            ProductArray response = new DatabaseController().searchProducts(body.getSearchString(), con);
-            return Response.ok().entity(response).build();
-        } catch (SQLException ex) {
-            return Response.status(400).entity(ex.toString()).build();
+        try {
+            AuthManagementClient.getNewInstance().checkAccessToken(TokenConverter.getToken(body.getToken()));
+            try (Connection con = DataSource.getInstance().getConnection()) {
+                ProductArray response = new DatabaseController().searchProducts(body.getSearchString(), con);
+                return Response.ok().entity(response).build();
+            } catch (SQLException e) {
+                Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+                return Response.status(400).entity(e.toString()).build();
+            }
+        } catch (io.swagger.client.ApiException e) {
+            Logger.getLogger(ProductsApiServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+            return Response.status(400).entity(e.toString()).build();
         }
     }
 }
